a74e8c18f340b696bed7d0c8f25951555d87f940
Lucas Harris <lucasharris.dev@outlook.com>
2023-04-26 16:14:32 -0400

    Add movement and animations

--- Assets/scripts/playermovement.cs
+++ Assets/scripts/playermovement.cs
@@ -5,20 +5,24 @@ using UnityEngine.InputSystem;
 
 public class playermovement : MonoBehaviour
 {
-    [SerializeField] float playerSpeed = 2.5f;
+    [SerializeField] float playerSpeed = 10f;
+    [SerializeField] float jumpHeight = 25f;
     Vector2 moveInput;
     Rigidbody2D myRigidBody;
+    Animator myAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
@@ -27,9 +31,28 @@ public class playermovement : MonoBehaviour
         Debug.Log(moveInput);
     }
 
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            myRigidBody.velocity = new Vector2(0f, jumpHeight);
+        }
+    }
+
     void Run()
     {
         Vector2 playerVelocity = new Vector2 (moveInput.x * playerSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+        myAnimator.SetBool("isRunning", PlayerHasHorizontalSpeed());
+    }
+
+    void FlipSprite()
+    {
+        transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f); // Sign returns 1 if positive, -1 if negative
+    }
+
+    bool PlayerHasHorizontalSpeed()
+    {
+        return Mathf.Abs(myRigidBody.velocity.x) >= Mathf.Epsilon; //Epsilon is the smallest non-zero float value
     }
 }
